ea1b55a7a414cbd5ddf24c7e7f385170b3e98813
Lorenzo Beltrame <lorenzo.beltrame@gmail.com>
2025-08-31 02:03:32 +0000

    Great changes but please fix the following, Overboard logo can stay the

--- src/components/brand-strip.tsx
+++ src/components/brand-strip.tsx
@@ -2,25 +2,23 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 const brands = [
-  { name: "Quiksilver", logo: "/images/logos/quiksilver.svg" },
-  { name: "Roxy", logo: "/images/logos/roxy.svg" },
-  { name: "Rip Curl", logo: "/images/logos/ripcurl.svg" },
-  { name: "O'Neill", logo: "/images/logos/oneill.svg" },
-  { name: "69Slam", logo: "/images/logos/69Slam.svg" },
-  { name: "Havaianas", logo: "/images/logos/havaianas.svg" },
-  { name: "Overboard", logo: "/images/logos/overboard.svg" },
-
-
+  { name: "Quiksilver", logo: "/images/logos/quiksilver.svg", invert: true },
+  { name: "Roxy", logo: "/images/logos/roxy.svg", invert: true },
+  { name: "Rip Curl", logo: "/images/logos/ripcurl.svg", invert: false },
+  { name: "O'Neill", logo: "/images/logos/oneill.svg", invert: true },
+  { name: "69Slam", logo: "/images/logos/69Slam.svg", invert: true },
+  { name: "Havaianas", logo: "/images/logos/havaianas.svg", invert: false },
+  { name: "Overboard", logo: "/images/logos/overboard.svg", invert: false },
 ];
 
-const Brand = ({ name, logo }: { name: string, logo: string }) => (
+const Brand = ({ name, logo, invert }: { name: string; logo: string; invert: boolean }) => (
   <li className="flex items-center justify-center mx-8">
-    <Image 
-      src={logo} 
+    <Image
+      src={logo}
       alt={`${name} logo`}
       width={120}
       height={40}
-      className="object-contain dark:invert"
+      className={cn("object-contain", invert && "dark:invert")}
     />
   </li>
 );
@@ -39,12 +37,12 @@ export function BrandStrip() {
         >
           <div className="animate-marquee group-hover:paused flex">
             {brands.map((brand) => (
-              <Brand key={brand.name} name={brand.name} logo={brand.logo} />
+              <Brand key={brand.name} {...brand} />
             ))}
           </div>
           <div className="animate-marquee-continuation group-hover:paused flex absolute top-0">
              {brands.map((brand) => (
-              <Brand key={`${brand.name}-2`} name={brand.name} logo={brand.logo} />
+              <Brand key={`${brand.name}-2`} {...brand} />
             ))}
           </div>
         </div>
